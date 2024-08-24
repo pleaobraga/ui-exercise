a9c695baac41df3dad0de1ffa9ba188498422b23
pleaobraga <pleao.braga@gmail.com>
2024-08-24 19:27:09 -0300

    chore: add coverage to the project

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,5 @@
 // vite.config.ts
-import { defineConfig } from 'vite'
+import { coverageConfigDefaults, defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
@@ -8,5 +8,14 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './vitest.setup.ts', // Point to your setup file for Vitest
+    coverage: {
+      exclude: [
+        '**/*.stories.@(js|jsx|mjs|ts|tsx)/**',
+        '**/*.mocks.ts',
+        'src/main.tsx',
+        'src/App.tsx',
+        ...coverageConfigDefaults.exclude,
+      ],
+    },
   },
 })
