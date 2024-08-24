56a27958be60c874c156b760cf79c47bb3414198
pleaobraga <pleao.braga@gmail.com>
2024-08-24 14:44:45 -0300

    feat: add responsivness to grid-card component

--- src/components/card-grid/card-grid.mocks.ts
+++ src/components/card-grid/card-grid.mocks.ts
@@ -17,5 +17,29 @@ export const CARD_MOCK_3 = {
   imageSrc: images[2].url,
   imageDimensions: images[2].dimensions,
 }
+export const CARD_MOCK_4 = {
+  id: 'CARD MOCK 4',
+  imageSrc: images[0].url,
+  imageDimensions: images[0].dimensions,
+}
+
+export const CARD_MOCK_5 = {
+  id: 'CARD MOCK 5',
+  imageSrc: images[1].url,
+  imageDimensions: images[1].dimensions,
+}
+
+export const CARD_MOCK_6 = {
+  id: 'CARD MOCK 6',
+  imageSrc: images[2].url,
+  imageDimensions: images[2].dimensions,
+}
 
 export const CARD_GRID_MOCK = [CARD_MOCK_1, CARD_MOCK_2, CARD_MOCK_3]
+
+export const CARD_GRID_MOCK_2_ROWS = [
+  ...CARD_GRID_MOCK,
+  CARD_MOCK_4,
+  CARD_MOCK_5,
+  CARD_MOCK_6,
+]
--- src/components/card-grid/card-grid.stories.tsx
+++ src/components/card-grid/card-grid.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react'
 import { CardGrid } from '.'
-import { CARD_GRID_MOCK } from './card-grid.mocks'
+import { CARD_GRID_MOCK, CARD_GRID_MOCK_2_ROWS } from './card-grid.mocks'
 
 const meta = {
   title: 'Components/Card Grid',
@@ -17,5 +17,17 @@ type Story = StoryObj<typeof meta>
 export const Default: Story = {
   args: {
     cards: [...CARD_GRID_MOCK],
+  },
+}
+
+export const TwoRows: Story = {
+  args: {
+    cards: [...CARD_GRID_MOCK_2_ROWS],
+  },
+}
+
+export const Empty: Story = {
+  args: {
+    cards: [],
   },
 }
