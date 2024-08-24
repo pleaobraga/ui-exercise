74a3498cba3c716059624673a0a735db0e5dae4c
pleaobraga <pleao.braga@gmail.com>
2024-08-24 12:18:20 -0300

    feat: add stats component to the card component

---
+++ src/components/card/card.constants.ts
@@ -0,0 +1,19 @@
+export const HEALTH_STATS = {
+  id: 'health',
+  name: 'health',
+  defaultValue: 100,
+}
+
+export const ATTACK_STATS = {
+  id: 'attack',
+  name: 'attack',
+  defaultValue: 0,
+}
+
+export const DEFENSE_STATS = {
+  id: 'defense',
+  name: 'defense',
+  defaultValue: 50,
+}
+
+export const CARD_STATS = [HEALTH_STATS, ATTACK_STATS, DEFENSE_STATS]
--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,17 +1,41 @@
 import './card.styles.scss'
 import { Stat } from './components/stat'
+import {
+  ATTACK_STATS,
+  CARD_STATS,
+  DEFENSE_STATS,
+  HEALTH_STATS,
+} from './card.constants'
+import { useState } from 'react'
 
 export function Card() {
+  const [statsValue, setStatsValue] = useState({
+    [HEALTH_STATS.id]: HEALTH_STATS.defaultValue,
+    [ATTACK_STATS.id]: ATTACK_STATS.defaultValue,
+    [DEFENSE_STATS.id]: DEFENSE_STATS.defaultValue,
+  })
+
+  const hanldeIncrement = (id: keyof typeof statsValue) => {
+    setStatsValue((prev) => ({ ...prev, [id]: prev[id] + 1 }))
+  }
+
+  const handleDecrement = (id: keyof typeof statsValue) => {
+    setStatsValue((prev) => ({ ...prev, [id]: prev[id] - 1 }))
+  }
+
   return (
     <div className="card">
       <div>Image box</div>
       <div className="card__content">
-        <Stat
-          name="test"
-          value={100}
-          onDecrement={() => {}}
-          onIncrement={() => {}}
-        />
+        {CARD_STATS.map(({ name, id }) => (
+          <Stat
+            key={id}
+            name={name}
+            value={statsValue[id]}
+            onDecrement={() => handleDecrement(id)}
+            onIncrement={() => hanldeIncrement(id)}
+          />
+        ))}
       </div>
     </div>
   )
