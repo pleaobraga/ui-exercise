9f3bcec6f11b36c5aa8a746bedf46224cca37cd0
pleaobraga <pleao.braga@gmail.com>
2024-08-25 18:57:32 -0300

    chore: add build configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/ui-exercise/',
   css: {
     modules: {
       // Customize the generated class names if needed
